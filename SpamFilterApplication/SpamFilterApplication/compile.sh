9350c3cf90553d34ceb34948227d274c7aabf576
Joel <x14fgg@gmail.com>
2017-11-23 12:53:00 -0500

    Experiment with dynamic directory names

--- SpamFilterApplication/SpamFilterApplication/compile.sh
+++ SpamFilterApplication/SpamFilterApplication/compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-mpiCC -o SpamFilter main.cpp Coordinator.cpp Worker.cpp EmailReader.cpp -pthread -Wno-deprecated -std=c++11 -lstdc++
+mpiCC -o SpamFilter_exe main.cpp Coordinator.cpp Worker.cpp EmailReader.cpp -pthread -Wno-deprecated -std=c++11 -lstdc++
 
-#For bsub
-bsub -n 4 -R "span[ptile=1]" -o %J.txt  "/opt/ibm/platform_mpi/bin/mpirun -lsf -vapi /home/nigelwilliams0708/Parallel-and-Distributed/T estMPISpamFilter/SpamFilter_exe /home/nigelwilliams0708/Parallel-and-Distributed/TestMPISpamFilt er/testXML.xml"
+# For bsub
+#bsub -n 4 -R "span[ptile=1]" -o %J.txt  "/opt/ibm/platform_mpi/bin/mpirun -lsf -vapi /home/nigelwilliams0708/Parallel-and-Distributed/T estMPISpamFilter/SpamFilter_exe /home/nigelwilliams0708/Parallel-and-Distributed/TestMPISpamFilt er/testXML.xml"
+
+# Test flexible version (working?)
+bsub -n 4 -R "span[ptile=1]" -o %J.txt  "/opt/ibm/platform_mpi/bin/mpirun -lsf -vapi $(pwd)/SpamFilter_exe $(pwd)/testXML.xml"
