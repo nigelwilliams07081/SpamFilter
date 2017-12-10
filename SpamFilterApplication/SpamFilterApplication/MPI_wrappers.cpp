ee85fd0d182e3d09f89a8fac000dc1016fcb75ed
taxicat1 <x14fgg@gmail.com>
2017-12-09 19:13:48 -0500

    Split MPI_wrappers into .h and .cpp

---
+++ SpamFilterApplication/SpamFilterApplication/MPI_wrappers.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+
+void MPI_Send_string(std::string s, int dest, int tag) {
+	int len = s.size();
+	
+	char* sc = new char[len + 1];
+	std::copy(s.begin(), s.end(), sc);
+	sc[len] = '\0'; // Need this null terminator
+	
+	MPI::COMM_WORLD.Send(sc, len + 1, MPI::UNSIGNED_CHAR, dest, tag);
+	delete[] sc;
+}
+
+void MPI_Send_bool(bool b, int dest, int tag) {
+	char val = (char)b;
+	MPI::COMM_WORLD.Send(&val, 1, MPI::UNSIGNED_CHAR, dest, tag);
+}
+
+std::string MPI_Recv_string(int source, int tag, MPI::Status &status) {
+	MPI::Status s;	
+	MPI::COMM_WORLD.Probe(source, tag, s);
+	int length = s.Get_count(MPI::UNSIGNED_CHAR);
+	
+	char* buffer = new char[length];
+	MPI::COMM_WORLD.Recv(buffer, length, MPI::UNSIGNED_CHAR, source, tag, status);
+	std::string result(buffer);
+	delete[] buffer;
+	return result;
+}
+
+std::string MPI_Recv_string(int source, int tag) {
+	MPI::Status s;	
+	MPI::COMM_WORLD.Probe(source, tag, s);
+	int length = s.Get_count(MPI::UNSIGNED_CHAR);
+	
+	char* buffer = new char[length];
+	MPI::COMM_WORLD.Recv(buffer, length, MPI::UNSIGNED_CHAR, source, tag);
+	std::string result(buffer);
+	delete[] buffer;
+	return result;
+}
+
+bool MPI_Recv_bool(int source, int tag, MPI::Status &status) {
+	char val;
+	MPI::COMM_WORLD.Recv(&val, 1, MPI::UNSIGNED_CHAR, source, tag, status);
+	return (bool)val;
+}
+
+bool MPI_Recv_bool(int source, int tag) {
+	char val;
+	MPI::COMM_WORLD.Recv(&val, 1, MPI::UNSIGNED_CHAR, source, tag);
+	return (bool)val;
+}
